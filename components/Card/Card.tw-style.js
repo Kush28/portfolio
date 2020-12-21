9a78dedad12924d93d05fe84a091cab9761b8a57
Kushal Mukherjee <kushal.mukherjee28exm@gmail.com>
2020-12-22 02:52:54 +0530

    Allignment fix

--- components/Card/Card.tw-style.js
+++ components/Card/Card.tw-style.js
@@ -1,16 +1,16 @@
 module.exports = {
   default: {
-    wrapper: '',
-    imgWrapper: 'min-h-postCard',
-    img: 'h-full w-auto object-cover rounded-md',
+    wrapper: 'max-h-200',
+    imgWrapper: 'min-h-100',
+    img: 'h-auto w-auto object-cover rounded-md',
     contentWrapper: 'mt-paragraph',
     title: 'text-xl mb-paragraph',
     text: 'hidden mb-paragraph',
     arrow: 'arrow',
   },
   featured: {
-    wrapper: 'flex',
-    imgWrapper: 'min-h-featureCard w-1/2',
+    wrapper: 'flex max-h-300',
+    imgWrapper: 'min-h-100 sm:min-h-200 w-1/2 h-200 sm:h-auto',
     img: 'h-full w-auto object-cover rounded-md',
     contentWrapper: 'w-1/2 mx-paragraph',
     title: 'text-2xl mb-paragraph',
