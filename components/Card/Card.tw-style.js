3d817e2dff96c92cf214f075ea3f95490357e368
Kushal Mukherjee <kushal.mukherjee28exm@gmail.com>
2020-12-23 00:42:28 +0530

    Card anim added

--- components/Card/Card.tw-style.js
+++ components/Card/Card.tw-style.js
@@ -1,20 +1,20 @@
 module.exports = {
   default: {
     wrapper: '',
-    imgWrapper: 'h-100 md:h-200 min-h-100',
-    img: 'h-full w-full object-cover rounded-md',
+    imgWrapper: 'h-100 md:h-200 min-h-100 overflow-hidden rounded-md',
+    img: 'h-full w-full object-cover transition duration-500 ease-in-out',
     contentWrapper: 'pt-paragraph',
     title: 'md:text-xl mb-paragraph',
     text: 'hidden mb-paragraph',
-    arrow: 'arrow',
+    arrow: 'arrow transition-all duration-100 ease-in-out',
   },
   featured: {
     wrapper: 'flex',
-    imgWrapper: 'min-h-100 sm:min-h-200 w-1/2 h-200 sm:h-auto max-h-300',
-    img: 'h-full w-auto object-cover rounded-md',
+    imgWrapper: 'min-h-100 sm:min-h-200 w-1/2 h-200 sm:h-auto max-h-300 overflow-hidden rounded-md',
+    img: 'h-full w-auto object-cover transition duration-500 ease-in-out',
     contentWrapper: 'w-1/2 px-paragraph',
     title: 'text-xl md:text-2xl mb-paragraph',
     text: 'text mb-paragraph hidden sm:block',
-    arrow: 'arrow',
+    arrow: 'arrow transition-all duration-100 ease-in-out',
   },
 }
